6f72f880847e55d831718561c691f5420b8eb462
YaniAR <58792454+YaniAR@users.noreply.github.com>
2020-10-26 03:09:59 +0000

    Update Constants.h

--- Terrain Generator/Header Files/Constants.h
+++ Terrain Generator/Header Files/Constants.h
@@ -8,6 +8,12 @@ constexpr int SCREEN_WIDTH{ 1024 };
 constexpr int SCREEN_HEIGHT{ 1024 };
 constexpr int PIXEL_WIDTH{ SCREEN_WIDTH / TERRAIN_SIZE };
 constexpr int PIXEL_HEIGHT{ SCREEN_HEIGHT / TERRAIN_SIZE };
-constexpr int TOTAL_ELEMENTS{ PIXEL_WIDTH * PIXEL_HEIGHT };
+
+// TERRAIN
+
+constexpr float SNOW_LEVEL = 0.6875f;
+constexpr float GRASS_LEVEL = 0.5625f;
+constexpr float SAND_LEVEL = 0.15625f;
+constexpr float WATER_LEVEL = 0.125f;
 
 #endif
