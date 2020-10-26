f3680df0f83a98c8dc08b8c1db76a5b2e808d80d
YaniAR <58792454+YaniAR@users.noreply.github.com>
2020-10-26 03:07:52 +0000

    Update Noise2d.cpp

--- Terrain Generator/Source Files/Noise2d.cpp
+++ Terrain Generator/Source Files/Noise2d.cpp
@@ -8,9 +8,9 @@ void generateNoise2d(int& nOctaves, float noise[], std::vector<float>& terrain,
 	if (nOctaves == 7)
 		nOctaves = 0;
 
-	for (int y = 0; y < PIXEL_HEIGHT; ++y)
+	for (int y = 0; y < TERRAIN_SIZE; ++y)
 	{
-		for (int x = 0; x < PIXEL_WIDTH; ++x)
+		for (int x = 0; x < TERRAIN_SIZE; ++x)
 		{
 			float Noise = 0.0f;
 			float ScaleAcc = 0.0f;
@@ -19,20 +19,20 @@ void generateNoise2d(int& nOctaves, float noise[], std::vector<float>& terrain,
 			for (int o = 0; o < nOctaves; o++)
 			{
 				// Shift by power of 2.
-				int samplePeriod = PIXEL_WIDTH >> o;
+				int samplePeriod = TERRAIN_SIZE >> o;
 				
 				// Get sample points
 				int xSample1 = (x / samplePeriod) * samplePeriod;
 				int ySample1 = (y / samplePeriod) * samplePeriod;
 
-				int xSample2 = (xSample1 + samplePeriod) % PIXEL_WIDTH; // wrap around x
-				int ySample2 = (ySample1 + samplePeriod) % PIXEL_WIDTH; // warp around y
+				int xSample2 = (xSample1 + samplePeriod) % TERRAIN_SIZE; // wrap around x
+				int ySample2 = (ySample1 + samplePeriod) % TERRAIN_SIZE; // warp around y
 
 				float xBlend = (float)(x - xSample1) / (float)samplePeriod;
 				float yBlend = (float)(y - ySample1) / (float)samplePeriod;
 
-				float Sample1 = Interpolate(terrain[ySample1 * PIXEL_WIDTH + xSample1], terrain[ySample1 * PIXEL_WIDTH + xSample2], xBlend);
-				float Sample2 = Interpolate(terrain[ySample2 * PIXEL_WIDTH + xSample1], terrain[ySample2 * PIXEL_WIDTH + xSample2], xBlend);
+				float Sample1 = Interpolate(terrain[ySample1 * TERRAIN_SIZE + xSample1], terrain[ySample1 * TERRAIN_SIZE + xSample2], xBlend);
+				float Sample2 = Interpolate(terrain[ySample2 * TERRAIN_SIZE + xSample1], terrain[ySample2 * TERRAIN_SIZE + xSample2], xBlend);
 
 				ScaleAcc += Scale;
 				Noise += (yBlend * (Sample2 - Sample1) + Sample1) * Scale;
@@ -40,77 +40,122 @@ void generateNoise2d(int& nOctaves, float noise[], std::vector<float>& terrain,
 			}
 
 			// Scale to seed range
-			noise[y * PIXEL_WIDTH + x] = Noise / ScaleAcc;
+			noise[y * TERRAIN_SIZE + x] = Noise / ScaleAcc;
 		}
 	}
 
 }
+/**/
 
-void drawNoise2d(float noise[], sf::RenderWindow& window, sf::Texture& texture)
+void bilinearInterpolate(float noise[], sf::RenderWindow& window, sf::Texture& texture)
 {
-	sf::Image image{};
 	sf::Sprite sprite{};
+	texture.update(window);
+	sf::Image image{ texture.copyToImage() };
 
-	image.create(PIXEL_WIDTH, PIXEL_HEIGHT, sf::Color(0, 0, 0));
-	sf::Color color{};
-	
-	for (int y{ 0 }; y < PIXEL_HEIGHT; ++y)
+	sf::Color tL{}, tR{}, bL{}, bR{};
+	sf::Vector2i tLPos{}, tRPos{}, bLPos{}, bRPos{};
+
+	for (int y{ 0 }; y < TERRAIN_SIZE * 2; ++y)
 	{
-		for (int x{ 0 }; x < PIXEL_WIDTH; ++x)
+		for (int x{ 0 }; x < TERRAIN_SIZE * 2; ++x)
 		{
-			int pixel_w{ (int)(noise[y * PIXEL_WIDTH + x] * 12.0f) };
-			switch (pixel_w)
+			getSquarePoints(bLPos, bRPos, tLPos, tRPos, bL, bR, tL, tR, x, y, image);
+			
+			int totalDistanceX{ tRPos.x - tLPos.x };
+			int totalDistanceY{ tRPos.y - bRPos.y };
+			int differenceToLeft{};
+			int differenceToRight{};
+			
+			sf::Color currentPixel{0, 0, 0};
+			for (int pixelY{ tLPos.y }; pixelY <= bLPos.y; ++pixelY)
 			{
-			case 0: // DARK BLUE
-				color = setColor(0, 119, 190);
-				break;
-			case 1:
-				color = setColor(0, 0, 139);
-				break;
-			case 2:
-				color = setColor(0, 0, 255);
-				break;
-			case 3:
-				color = setColor(0, 0, 205);
-				break;
-			case 4:
-				color = setColor(0, 105, 148);
-				break;
-			case 5: // GREEN
-				color = setColor(34, 139, 34);
-				break;
-			case 6:
-				color = setColor(0, 128, 0);
-				break;
-			case 7:
-				color = setColor(0, 100, 0);
-				break;
-			case 8:
-				color = setColor(124, 252, 0);
-				break;
-			case 9: // GREY
-				color = setColor(194, 178, 128);
-				break;
-			case 10: // BROWN
-				color = setColor(139, 69, 19);
-				break;
-			case 11:
-				color = setColor(160, 82, 45);
-				break;
-			case 12:
-				color = setColor(218, 165, 32);
-				break;
-			default:
-				color = setColor(0, 0, 0);
-				break;
+				for (int pixelX{ tLPos.x }; pixelX <= tRPos.x; ++pixelX)
+				{
+					differenceToLeft = pixelX - tLPos.x;
+					differenceToRight = tRPos.x - pixelX;
+					
+					int differenceToTop{ tLPos.y - pixelY };
+					int differenceToBottom{ pixelY - bLPos.y };
+
+					int rTop = (tL.r * differenceToRight + tR.r * differenceToLeft) / totalDistanceX;
+					int gTop = (tL.g * differenceToRight + tR.g * differenceToLeft) / totalDistanceX;
+					int bTop = (tL.b * differenceToRight + tR.b * differenceToLeft) / totalDistanceX;
+
+					int rBot = (bL.r * differenceToRight + bR.r * differenceToLeft) / totalDistanceX;
+					int gBot = (bL.g * differenceToRight + bR.g * differenceToLeft) / totalDistanceX;
+					int bBot = (bL.b * differenceToRight + bR.b * differenceToLeft) / totalDistanceX;
+
+					currentPixel.r = static_cast<sf::Uint8>((rBot * differenceToTop + rTop * differenceToBottom) / totalDistanceY);
+					currentPixel.g = static_cast<sf::Uint8>((gBot * differenceToTop + gTop * differenceToBottom) / totalDistanceY);
+					currentPixel.b = static_cast<sf::Uint8>((bBot * differenceToTop + bTop * differenceToBottom) / totalDistanceY);
+
+					image.setPixel(pixelX, pixelY, currentPixel);
+				}
 			}
-			//color = FloatToColour(noise[y * PIXEL_WIDTH + x]) -> for natural noise
-			image.setPixel(x, y, color);
 		}
 	}
+	std::cout << "done\n";
 
 	texture.update(image);
 	sprite.setTexture(texture);
-	sprite.setScale(PIXEL_WIDTH, PIXEL_HEIGHT);
 	window.draw(sprite);
+}
+
+void drawDots(sf::RenderWindow& window)
+{
+	sf::RectangleShape dot(sf::Vector2f(LINETHICKNESS, LINETHICKNESS));
+	dot.setFillColor(sf::Color::Black);
+
+	for (int y{ 0 }; y < TERRAIN_SIZE * 2; ++y)
+	{
+		for (int x{ 0 }; x < TERRAIN_SIZE * 2; ++x)
+		{
+			dot.setPosition(static_cast<float>(((TERRAIN_SIZE * x + TERRAIN_SIZE) / 2) - TERRAIN_SIZE / 4), 
+				static_cast<float>(((TERRAIN_SIZE * y + TERRAIN_SIZE) / 2) - TERRAIN_SIZE / 4));
+			window.draw(dot);
+		}
+	}
+}
+
+void drawNoise2d(float noise[], sf::RenderWindow& window, sf::Texture& texture, bool interpolate)
+{
+	sf::Color color{};
+
+	sf::RectangleShape cell{ sf::Vector2f(TERRAIN_SIZE / 2, TERRAIN_SIZE / 2) };
+	float pixel_w{};
+	for (int y{ 0 }; y < TERRAIN_SIZE * 2; ++y)
+	{
+		for (int x{ 0 }; x < TERRAIN_SIZE * 2; ++x)
+		{
+			pixel_w = nearestNeighbourInterpolate(noise, x, y);
+			pixel_w = std::pow(pixel_w, 2);
+			if (WATER_LEVEL > pixel_w)
+			{
+				color = setColor(0, std::round(127 * (pixel_w / WATER_LEVEL)), 192 + std::round(63 * (pixel_w - WATER_LEVEL)));
+			}
+			else if (SAND_LEVEL > pixel_w)
+			{
+				color = setColor(255, 255, 63);
+			}
+			else if (GRASS_LEVEL > pixel_w)
+			{
+				color = setColor(0, 255 - std::round(128 * ((pixel_w - SAND_LEVEL) / GRASS_LEVEL)), 64 - std::round(31 * ((pixel_w - SAND_LEVEL) / GRASS_LEVEL)));
+			}
+			else if (SNOW_LEVEL > pixel_w)
+			{
+				color = setColor(95, 64 + std::round(31 * ((pixel_w - GRASS_LEVEL) / SNOW_LEVEL)), 64 + std::round(31 * ((pixel_w - GRASS_LEVEL) / SNOW_LEVEL)));
+			}
+			else
+			{
+				color = setColor(192 + std::round(63 * ((pixel_w - SNOW_LEVEL) / SNOW_LEVEL)), 
+					192 + std::round(63 * (pixel_w - SNOW_LEVEL)), 
+					192 + std::round(63 * (pixel_w - SNOW_LEVEL)));
+			}
+
+			cell.setFillColor(color);
+			cell.setPosition(x * (PIXEL_WIDTH / 2), y * (PIXEL_HEIGHT / 2));
+			window.draw(cell);
+		}
+	}
 }
